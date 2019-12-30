1c527664d5a83c20a422754e7d114816895c08c9
codiechanel <codiechanel@gmail.com>
2019-12-30 16:40:24 +0800

    hjvjk

--- src/common/Store.ts
+++ src/common/Store.ts
@@ -1,23 +1,28 @@
-import {computed, observable, observe, runInAction} from 'mobx'
-import {
-  RemoteMongoClient,
-  UserPasswordCredential
-} from 'mongodb-stitch-browser-sdk'
-import { app } from './stitch'
+import {action, computed, observable} from 'mobx'
 // import api from "../common/api";
-import axios from "axios";
-import { action } from 'mobx'
 import api from './Api'
+import * as dayjs from 'dayjs'
+import * as relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime)
 let GRAPH_ENDPOINT = "https://graph-express.herokuapp.com/graphql";
 
 class Store {
   @observable keyword = '';
+  @observable
+  selectedSort = 'noSort'
   @observable
   selectedTagId = null
   packages = observable.map(new Map(), { deep: false })
   allPackages = observable.map(new Map(), { deep: false })
   // categories = observable.map(new Map(), { deep: false });
   tags = observable.map(new Map(), { deep: false })
+
+
+  @action
+  changeSort(sortId) {
+    this.selectedSort = sortId
+  }
 
   async addPackage(name) {
     await api.addPackage(name)
@@ -38,7 +43,53 @@ class Store {
 
   @computed
   get packagesArray() {
-    return Array.from(this.packages)
+    let list: any = Array.from(this.packages)
+
+    if (this.selectedSort == 'monthlyDownloads') {
+      list = list.sort(function(a, b) {
+        let [keyA, valA] = a
+        let [keyB, valB] = b
+        if (valA.npm && valB.npm) {
+          let downloadsA = valA.npm.downloads[2].count
+          let downloadsB = valB.npm.downloads[2].count
+          /* sort desc */
+          return downloadsB - downloadsA
+        } else {
+          return 0
+        }
+      })
+    } else if (this.selectedSort == 'yearlyDownloads') {
+      list = list.sort(function(a, b) {
+        let [keyA, valA] = a
+        let [keyB, valB] = b
+        if (valA.npm && valB.npm) {
+          let downloadsA = valA.npm.downloads[5].count
+          let downloadsB = valB.npm.downloads[5].count
+          /* sort desc */
+          return downloadsB - downloadsA
+        } else {
+          return 0
+        }
+      })
+    }
+    else if (this.selectedSort == 'sortByYear') {
+      list = list.sort(function(a, b) {
+        let [keyA, valA] = a
+        let [keyB, valB] = b
+        if (valA.githubExtra && valB.githubExtra) {
+          let createdA = valA.githubExtra.created_at
+          createdA = dayjs(createdA).unix()
+          let createdB = valB.githubExtra.created_at
+          createdB = dayjs(createdB).unix()
+          /* in unix time since epoch
+          the more recent date has a greater integer value  */
+          return createdA - createdB
+        } else {
+          return 0
+        }
+      })
+    }
+    return list
 
   }
 
--- src/components/Packages.tsx
+++ src/components/Packages.tsx
@@ -1,30 +1,34 @@
 import store from '../common/Store'
 import * as React from 'react'
-import { observer } from 'mobx-react'
-import { List, ListItem , CollapsibleList, SimpleListItem} from '@rmwc/list'
-import { useEffect, useState } from 'react'
-import styled, { createGlobalStyle } from 'styled-components'
-import { CircularProgress } from '@rmwc/circular-progress'
+import {observer} from 'mobx-react'
+import {List, ListItem, CollapsibleList, SimpleListItem} from '@rmwc/list'
+import {useEffect, useState} from 'react'
+import styled, {createGlobalStyle} from 'styled-components'
+import {CircularProgress} from '@rmwc/circular-progress'
 import * as dayjs from 'dayjs'
 import * as relativeTime from 'dayjs/plugin/relativeTime'
-import {Chip,ChipSet} from '@rmwc/chip'
-import { Typography } from '@rmwc/typography'
+import {Chip, ChipSet} from '@rmwc/chip'
+import {Typography} from '@rmwc/typography'
 import '@rmwc/list/collapsible-list.css';
 import api from "../common/Api";
+import {Select} from '@rmwc/select'
+
 dayjs.extend(relativeTime)
+
 function useLoader() {
-  const [data, setData] = useState('loading')
-  useEffect(() => {
-    // Update the document title using the browser API
-    // store.fetchNews();
-    const fetchData = async () => {
-      await store.fetchPackages()
-      setData('done')
-    }
-    fetchData()
-  }, [])
-  return data
+    const [data, setData] = useState('loading')
+    useEffect(() => {
+        // Update the document title using the browser API
+        // store.fetchNews();
+        const fetchData = async () => {
+            await store.fetchPackages()
+            setData('done')
+        }
+        fetchData()
+    }, [])
+    return data
 }
+
 export const FullHeight = styled.div`
   display: flex;
   flex: 1;
@@ -48,67 +52,100 @@ export const Box = styled.div`
 const StyledTypography = styled(Typography)`
   color: steelblue;
 `;
+
 function Packages(props) {
-  let data = useLoader()
-  // let list: any = Array.from(store.packages)
-  //   let content = <div>hello</div>
-  let content = (
-    <CenteredFlex>
-      <CircularProgress />
-    </CenteredFlex>
-  )
-  if (data === 'done') {
-    content = (
-      <List>
-        {store.packagesArray.map(([key, item]) => {
-          let downloadsLastMonth = null
-          let downloadsLastYear
-          if (item.npm) {
-             downloadsLastYear = api.formatNumber(item.npm.downloads[5].count)
-
-             downloadsLastMonth = api.formatNumber(item.npm.downloads[2].count)
-          }
-
-          let starsCount = null
-          if (item.github) {
-            starsCount = api.formatNumber( item.github.starsCount)
-          }
-          let created_at = null
-          if (item.githubExtra) {
-            created_at = item.githubExtra.created_at
-            created_at = dayjs(created_at)
-            // @ts-ignore
-            created_at = dayjs().from(created_at, true) + ' ago'
-          }
-          // @ts-ignore
-          // @ts-ignore
-          return (
-            <CollapsibleList  key={key}
-                handle={
-                  <SimpleListItem
-                      text={item.name}
-                      graphic="favorite"
-                      metaIcon="chevron_right"
-                  />
-                }
-                onOpen={() => console.log('open')}
-                onClose={() => console.log('close')}
-            >
-              <ChipSet>
-              { starsCount && <Chip label="stars" > <Typography style={{color:"magenta"}} use="headline6" >{starsCount}</Typography> </Chip> }
-                { created_at && <Chip label="created_at" > <Typography style={{color:"magenta"}} use="headline6" >{created_at}</Typography></Chip> }
-                { downloadsLastMonth && <Chip icon={"cloud_download"} label="LastMonth" > <Typography style={{color:"magenta"}} use="headline6" >{downloadsLastMonth}</Typography></Chip> }
-                { downloadsLastYear && <Chip icon={"cloud_download"} label="LastYear" > <Typography style={{color:"magenta"}} use="headline6" >{downloadsLastYear}</Typography></Chip> }
-
-              </ChipSet>
-            </CollapsibleList>
-          // <ListItem key={key}>{item.name}</ListItem>
-        ) })}
-      </List>
+    let data = useLoader()
+    // let list: any = Array.from(store.packages)
+    //   let content = <div>hello</div>
+    let content = (
+        <CenteredFlex>
+            <CircularProgress/>
+        </CenteredFlex>
     )
-  }
+    if (data === 'done') {
+        content = (
+            <List>
+                {store.packagesArray.map(([key, item]) => {
+                    let downloadsLastMonth = null
+                    let downloadsLastYear
+                    if (item.npm) {
+                        downloadsLastYear = api.formatNumber(item.npm.downloads[5].count)
 
-  return <FullHeight> {content}</FullHeight>
+                        downloadsLastMonth = api.formatNumber(item.npm.downloads[2].count)
+                    }
+
+                    let starsCount = null
+                    if (item.github) {
+                        starsCount = api.formatNumber(item.github.starsCount)
+                    }
+                    let created_at = null
+                    if (item.githubExtra) {
+                        created_at = item.githubExtra.created_at
+                        created_at = dayjs(created_at)
+                        // @ts-ignore
+                        created_at = dayjs().from(created_at, true) + ' ago'
+                    }
+                    // @ts-ignore
+                    // @ts-ignore
+                    return (
+                        <CollapsibleList key={key}
+                                         handle={
+                                             <SimpleListItem
+                                                 text={item.name}
+                                                 graphic="favorite"
+                                                 metaIcon="chevron_right"
+                                             />
+                                         }
+                                         onOpen={() => console.log('open')}
+                                         onClose={() => console.log('close')}
+                        >
+                            <ChipSet>
+                                {starsCount && <Chip label="stars"> <Typography style={{color: "magenta"}}
+                                                                                use="headline6">{starsCount}</Typography>
+                                </Chip>}
+                                {created_at && <Chip label="created_at"> <Typography style={{color: "magenta"}}
+                                                                                     use="headline6">{created_at}</Typography></Chip>}
+                                {downloadsLastMonth &&
+                                <Chip icon={"cloud_download"} label="LastMonth"> <Typography style={{color: "magenta"}}
+                                                                                             use="headline6">{downloadsLastMonth}</Typography></Chip>}
+                                {downloadsLastYear &&
+                                <Chip icon={"cloud_download"} label="LastYear"> <Typography style={{color: "magenta"}}
+                                                                                            use="headline6">{downloadsLastYear}</Typography></Chip>}
+
+                            </ChipSet>
+                        </CollapsibleList>
+                        // <ListItem key={key}>{item.name}</ListItem>
+                    )
+                })}
+            </List>
+        )
+    }
+
+
+    return <FullHeight>
+        <SortPanel/>
+
+        {content}</FullHeight>
 }
 
+const SortPanel = observer (props => {
+    console.log('render')
+    const options = [
+        {
+            label: 'noSort',
+            value: 'noSort'
+        },
+        {
+            label: 'sortByYear',
+            value: 'sortByYear',
+
+        },
+        {
+            label: 'monthlyDownloads',
+            value: 'monthlyDownloads'
+        }
+    ];
+    return  <div style={{height: 100}}><Select enhanced value={store.selectedSort} onChange={(e) => store.changeSort(e.currentTarget.value)} label="Standard"
+                         options={options}/></div>
+})
 export default observer(Packages)
